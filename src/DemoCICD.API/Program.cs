c184f6fc067d16f4aba90bf7c00cb5740bd3f6dd
trung.tq <trung.tq@TRUNGTQ-SS-HCM>
2025-09-20 17:49:20 +0700

    test

--- src/DemoCICD.API/Program.cs
+++ src/DemoCICD.API/Program.cs
@@ -49,7 +49,7 @@ builder.Services.AddCarter();
 
 // Cấu hình Dapper
 builder.Services.AddInfrastructureDapper();
-//test webhook CICD
+//test webhook CICD 2
 builder.Services
         .AddSwaggerGenNewtonsoftSupport()
         .AddFluentValidationRulesToSwagger()
