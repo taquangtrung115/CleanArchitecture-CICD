73be5908210f00651599e4ea01bb65492ace0ddd
trung.tq <trung.tq@TRUNGTQ-SS-HCM>
2025-09-20 21:58:48 +0700

    merge

--- src/DemoCICD.Persistence/Migrations/20250920145741_InitialMigration_4.Designer.cs
+++ src/DemoCICD.Persistence/Migrations/20250920145741_InitialMigration_4.Designer.cs
@@ -4,6 +4,7 @@ using DemoCICD.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DemoCICD.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250920145741_InitialMigration_4")]
+    partial class InitialMigration_4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/DemoCICD.Persistence/Migrations/20250920145741_InitialMigration_4.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoCICD.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class InitialMigration_4 : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "Address",
+            table: "AspNetUsers",
+            type: "nvarchar(max)",
+            nullable: true);
+
+        migrationBuilder.AddColumn<string>(
+            name: "Avatar",
+            table: "AspNetUsers",
+            type: "nvarchar(max)",
+            nullable: true);
+
+        migrationBuilder.AddColumn<string>(
+            name: "Bio",
+            table: "AspNetUsers",
+            type: "nvarchar(max)",
+            nullable: true);
+
+        migrationBuilder.AddColumn<string>(
+            name: "City",
+            table: "AspNetUsers",
+            type: "nvarchar(max)",
+            nullable: true);
+
+        migrationBuilder.AddColumn<string>(
+            name: "Country",
+            table: "AspNetUsers",
+            type: "nvarchar(max)",
+            nullable: true);
+
+        migrationBuilder.AddColumn<string>(
+            name: "Phone",
+            table: "AspNetUsers",
+            type: "nvarchar(max)",
+            nullable: true);
+
+        migrationBuilder.AddColumn<string>(
+            name: "Website",
+            table: "AspNetUsers",
+            type: "nvarchar(max)",
+            nullable: true);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "Address",
+            table: "AspNetUsers");
+
+        migrationBuilder.DropColumn(
+            name: "Avatar",
+            table: "AspNetUsers");
+
+        migrationBuilder.DropColumn(
+            name: "Bio",
+            table: "AspNetUsers");
+
+        migrationBuilder.DropColumn(
+            name: "City",
+            table: "AspNetUsers");
+
+        migrationBuilder.DropColumn(
+            name: "Country",
+            table: "AspNetUsers");
+
+        migrationBuilder.DropColumn(
+            name: "Phone",
+            table: "AspNetUsers");
+
+        migrationBuilder.DropColumn(
+            name: "Website",
+            table: "AspNetUsers");
+    }
+}
