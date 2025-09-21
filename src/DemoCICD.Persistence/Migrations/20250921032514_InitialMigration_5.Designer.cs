eef95f489b992e1a7aed6e491f55d036c5ed4a7a
trung.tq <trung.tq@TRUNGTQ-SS-HCM>
2025-09-21 10:25:49 +0700

    merge

--- src/DemoCICD.Persistence/Migrations/20250921032514_InitialMigration_5.Designer.cs
+++ src/DemoCICD.Persistence/Migrations/20250921032514_InitialMigration_5.Designer.cs
@@ -4,6 +4,7 @@ using DemoCICD.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DemoCICD.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250921032514_InitialMigration_5")]
+    partial class InitialMigration_5
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/DemoCICD.Persistence/Migrations/20250921032514_InitialMigration_5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoCICD.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class InitialMigration_5 : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "PasswordResetTokens",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                ResetCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                ExpiresAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                IsUsed = table.Column<bool>(type: "bit", nullable: false),
+                UsedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_PasswordResetTokens", x => x.Id);
+            });
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "PasswordResetTokens");
+    }
+}
